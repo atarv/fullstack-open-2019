ffb637a678a59578ac1ec9695e3d20a4d8285b6a
atarv <a.aleksi.tarvainen@gmail.com>
2019-05-24 11:52:20 +0300

    Osa 2 maiden tiedot vaihe 3 valmis

--- osa2/maiden-tiedot/src/App.js
+++ osa2/maiden-tiedot/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import APIKey from './APIKey' // OpenWeatherAPIKey
 import './App.css'
 
-const SearchField = ({ entry, setEntry }) => {
+const SearchField = ({ setEntry }) => {
     const search = event => {
         setEntry(event.target.value)
     }
@@ -15,24 +16,52 @@ const SearchField = ({ entry, setEntry }) => {
 }
 
 const FullData = ({ country }) => {
+    const [temp, setTemp] = useState(0)
+    const [desc, setDesc] = useState('')
+    const [wind, setWind] = useState('')
+    const [icon, setIcon] = useState('')
     const listLanguages = country =>
         country.languages.map((lang, i) => <li key={i}>{lang.name}</li>)
-    const showData = () => {
+    const weatherQuery = city => {
+        axios
+            .get(
+                `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey.OpenWeatherAPIKey}`
+            )
+            .then(response => {
+                setTemp((response.data.main.temp - 273.15).toFixed(2))
+                setDesc(response.data.weather[0].description)
+                setWind(response.data.wind.speed + 'm/s, ' + response.data.wind.deg + ' degrees')
+                setIcon(`http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`)
+            })
         return (
             <div>
-                <h1>{country.name}</h1>
-                <p>
-                    Capital: {country.capital}
-                    <br />
-                    Population: {country.population}
-                </p>
-                <h2>Languages</h2>
-                <ul>{listLanguages(country)}</ul>
-                <img src={country.flag} alt={country.demonym + ' flag'} height={200} />
+                <h2>Weather in {country.capital}</h2>
+                <div>
+                    <b>Temperature: </b>
+                    {/* Kelvins converted to celsius */}
+                    {temp}&#x2103;
+                </div>
+                <img src={icon} alt="icon"/>
+                <div><b>Description: </b>{desc}</div>
+                <div><b>Wind: </b>{wind}</div>
             </div>
         )
     }
-    return <div>{showData()}</div>
+
+    return (
+        <div>
+            <h1>{country.name}</h1>
+            <p>
+                Capital: {country.capital}
+                <br />
+                Population: {country.population}
+            </p>
+            <h2>Languages</h2>
+            <ul>{listLanguages(country)}</ul>
+            <img src={country.flag} alt={country.demonym + ' flag'} height={200} />
+            {weatherQuery(country.capital)}
+        </div>
+    )
 }
 
 const PartialData = ({ country, setEntry }) => {
@@ -70,7 +99,7 @@ function App() {
 
     return (
         <div>
-            <SearchField entry={entry} setEntry={setEntry} />
+            <SearchField setEntry={setEntry} />
             <CountryData entry={entry} countries={countries} setEntry={setEntry} />
         </div>
     )
