1e6bac5de91889ad5ef31374013a4a12beca759d
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-24 10:00:40 +0300

    Osa 2 maiden tiedot, kesken

---
+++ osa2/maiden-tiedot/src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const SearchField = ({ entry, setEntry }) => {
+    const search = event => {
+        setEntry(event.target.value)
+    }
+
+    return (
+        <div>
+            Find countries <input onChange={search} />
+        </div>
+    )
+}
+
+const FullData = ({ country }) => {
+    const listLanguages = country =>
+        country.languages.map((lang, i) => <li key={i}>{lang.name}</li>)
+    const showData = () => {
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <p>
+                    Capital: {country.capital}
+                    <br />
+                    Population: {country.population}
+                </p>
+                <h2>Languages</h2>
+                <ul>{listLanguages(country)}</ul>
+                <img src={country.flag} alt={country.demonym + ' flag'} height={200} />
+            </div>
+        )
+    }
+    return <div>{showData()}</div>
+}
+
+const PartialData = ({ country, setEntry }) => {
+    return (
+        <div key={country.name}>
+            {country.name} <button onClick={() => setEntry(country.name)}>show</button>
+        </div>
+    )
+}
+
+const CountryData = ({ entry, countries, setEntry }) => {
+    if (countries.length === 0) return <div />
+    const matchingCountries = countries.filter(country =>
+        country.name.toLowerCase().includes(entry.toLowerCase())
+    )
+    if (matchingCountries.length === 1) return <FullData country={matchingCountries[0]} />
+    else if (matchingCountries.length <= 10)
+        return matchingCountries.map(country => (
+            <PartialData key={country.name} country={country} setEntry={setEntry} />
+        ))
+    else return <div>Too many matches, specify another filter</div>
+}
+
+function App() {
+    const [entry, setEntry] = useState('')
+    const [countries, setCountries] = useState([])
+
+    const getCountries = () => {
+        axios.get('https://restcountries.eu/rest/v2/all').then(response => {
+            setCountries(response.data)
+        })
+    }
+
+    useEffect(getCountries, [])
+
+    return (
+        <div>
+            <SearchField entry={entry} setEntry={setEntry} />
+            <CountryData entry={entry} countries={countries} setEntry={setEntry} />
+        </div>
+    )
+}
+
+export default App
