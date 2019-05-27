3b02d2f372b9ae0c3fe8aad3d1adc891a1d4ee44
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-27 11:56:11 +0300

    Osa 3 puhelinluettelo vaihe 1

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 /* Full-stack open 2019 Osa 3 puhelinluettelo */
-import express from 'express';
+const express = require('express')
 const app = express()
 
+let persons = [
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "045-1236543"
+    },
+    {
+        id: 2,
+        name: "Arto Järvinen",
+        number: "041-21423123"
+    },
+    {
+        id: 3,
+        name: "Lea Kutvonen",
+        number: "040-04323234"
+    },
+    {
+        id: 4,
+        name: "Martti Tienari",
+        number: "09-784232"
+
+    }
+]
+
+app.get('/api/persons', (req, response) => {
+    response.json(persons)
+})
+
 const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
