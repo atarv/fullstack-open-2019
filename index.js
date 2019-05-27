3c0e1cd08cd96cc1edec6d2c5bda270b547e1635
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-27 13:34:25 +0300

    Osa 3, puhelinluettelo vaihe 6

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* Full-stack open 2019 Osa 3 puhelinluettelo */
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
     {
@@ -36,6 +39,34 @@ app.get('/api/persons/:id', (req, res) => {
         return res.status(404).end()
     }
     res.json(person)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    console.log(person)
+
+    if (!person) {
+        return res.status(404).end()
+    }
+    persons = persons.filter(p => p.id != id)
+    console.log(persons)
+
+    res.end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const newPerson = req.body
+    if (persons.find(p => p.name === newPerson.name)) {
+        return res.status(400).json({ error: 'Name must be unique' })
+    } else if (!newPerson.name || !newPerson.number) {
+        return res.status(400).json({error: 'Person must have both a name and a number'})
+    }
+
+    const newId = Math.ceil(Math.random() * 9000000)
+    newPerson.id = newId
+    persons = persons.concat(newPerson)
+    res.json(newPerson)
 })
 
 app.get('/info', (req, res) => {
