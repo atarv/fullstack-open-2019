c54632cf3f363b96c866774caca08b8bfe6261a7
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-27 12:21:39 +0300

    Osa 3, puhelinluettelo vaihe 3

--- index.js
+++ index.js
@@ -25,14 +25,23 @@ let persons = [
     }
 ]
 
-app.get('/api/persons', (req, response) => {
-    response.json(persons)
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
 })
 
-app.get('/info', (req, response) => {
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (!person) {
+        return res.status(404).end()
+    }
+    res.json(person)
+})
+
+app.get('/info', (req, res) => {
     const date = new Date()
-    response.charset = 'UTF-8'
-    response.send(`<p>Puhelinluettelossa on ${persons.length} nimeä</p><p>${date}</p>`)
+    res.charset = 'UTF-8'
+    res.send(`<p>Puhelinluettelossa on ${persons.length} nimeä</p><p>${date}</p>`)
 })
 
 const PORT = 3001
