8186141687159c6d93d1be5fb59e672caf5bc921
aleksi <a.aleksi.tarvainen@gmail.com>
2019-05-27 12:09:49 +0300

    Osa 3 puhelinluettelo vaihe 2

--- index.js
+++ index.js
@@ -5,29 +5,34 @@ const app = express()
 let persons = [
     {
         id: 1,
-        name: "Arto Hellas",
-        number: "045-1236543"
+        name: 'Arto Hellas',
+        number: '045-1236543'
     },
     {
         id: 2,
-        name: "Arto Järvinen",
-        number: "041-21423123"
+        name: 'Arto Järvinen',
+        number: '041-21423123'
     },
     {
         id: 3,
-        name: "Lea Kutvonen",
-        number: "040-04323234"
+        name: 'Lea Kutvonen',
+        number: '040-04323234'
     },
     {
         id: 4,
-        name: "Martti Tienari",
-        number: "09-784232"
-
+        name: 'Martti Tienari',
+        number: '09-784232'
     }
 ]
 
 app.get('/api/persons', (req, response) => {
     response.json(persons)
+})
+
+app.get('/info', (req, response) => {
+    const date = new Date()
+    response.charset = 'UTF-8'
+    response.send(`<p>Puhelinluettelossa on ${persons.length} nimeä</p><p>${date}</p>`)
 })
 
 const PORT = 3001
