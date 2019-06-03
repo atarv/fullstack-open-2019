028d1c7b9171ff6dfea3b92eef3e3f83e8640f44
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-03 10:49:37 +0300

    Projektin jakaminen moduuleihin

---
+++ osa4/app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api/blogs', blogsRouter)
+
+
+module.exports = app
