ac67ac45178eb4a354e9b5d281f4d0361f7ef061
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-04 15:37:57 +0300

    Blogilistan testit laajennoksineen

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -7,12 +7,35 @@ blogsRouter.get('/', (request, response) => {
     })
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
+    try {
+        const res = await blog.save()
+        // console.log(res.hasWriteError(), res.writeError)
 
-    blog.save().then(result => {
-        response.status(201).json(result)
-    })
+        // if (res.hasWriteError()) throw res.writeError
+        response.status(201).json(res)
+    } catch (e) {
+        response.status(400).end()
+    }
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+    } catch (error) {
+        response.status(404).end()
+    }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndUpdate(request.params.id)
+        response.status(204).end()
+    } catch (error) {
+        response.status(404).end()
+    }
 })
 
 module.exports = blogsRouter
