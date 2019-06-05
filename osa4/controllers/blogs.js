a04765a5d11fbb19a825e9537f8e330610db26d5
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-05 17:12:39 +0300

    Osa 4 valmis

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('userId', { blogs: 0 })
@@ -9,16 +10,22 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const user = await User.findById(body.userId)
 
-    const blog = new Blog({
-        author: body.author,
-        likes: body.likes,
-        title: body.title,
-        url: body.url,
-        userId: user._id
-    })
     try {
+        const decodedToken = jwt.verify(request.token, process.env.SECRET)
+        if (!request.token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        const user = await User.findById(body.userId)
+
+        const blog = new Blog({
+            author: body.author,
+            likes: body.likes,
+            title: body.title,
+            url: body.url,
+            userId: user._id
+        })
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
@@ -31,10 +38,21 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
     try {
-        await Blog.findByIdAndDelete(request.params.id)
-        response.status(204).end()
+        const toBeDeletedBlog = await Blog.findById(request.params.id)
+        if (toBeDeletedBlog.userId.toString() === decodedToken.id.toString()) {
+            await Blog.deleteOne(toBeDeletedBlog)
+            response.status(204).end()
+        } else {
+            response.status(401).json({ error: 'token invalid or missing' })
+        }
     } catch (error) {
+        console.log(error)
         response.status(404).end()
     }
 })
