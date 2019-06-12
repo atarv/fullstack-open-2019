8e0e47874b8a7a1b904d0450525e949e309cc0f1
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-12 13:52:58 +0300

    Osa 6, anekdootit ja backend, vaihe 1

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,10 +1,10 @@
 const anecdotesAtStart = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+    // 'If it hurts, do it more often',
+    // 'Adding manpower to a late software project makes it later!',
+    // 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    // 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    // 'Premature optimization is the root of all evil.',
+    // 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -14,6 +14,13 @@ const asObject = anecdote => {
         content: anecdote,
         id: getId(),
         votes: 0
+    }
+}
+
+export const initializeAnecdotes = anecdotes => {
+    return {
+        type: 'INIT',
+        data: anecdotes
     }
 }
 
@@ -41,6 +48,8 @@ const reducer = (state = initialState, action) => {
     // console.log('state now: ', state)
     console.log('action', action)
     switch (action.type) {
+        case 'INIT':
+            return action.data
         case 'VOTE':
             return [...state].map(anecdote =>
                 anecdote.id !== action.data.id
