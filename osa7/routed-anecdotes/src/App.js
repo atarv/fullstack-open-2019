6e84a5accd45ae72e99f6a2a5e00cd3978c32703
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-14 11:22:56 +0300

    Osa 7, routed anecdotes, vaihe 2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -25,9 +25,21 @@ const AnecdoteList = ({ anecdotes }) => (
         <h2>Anecdotes</h2>
         <ul>
             {anecdotes.map(anecdote => (
-                <li key={anecdote.id}>{anecdote.content}</li>
+                <li key={anecdote.id}>
+                    <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+                </li>
             ))}
         </ul>
+    </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+    <div>
+        <h2>{anecdote.content}</h2>
+        <p>has {anecdote.votes} votes</p>
+        <p>
+            for more info see <a href={anecdote.info}>{anecdote.info}</a>
+        </p>
     </div>
 )
 
@@ -150,6 +162,11 @@ const App = () => {
                 <Route exact path="/" render={() => <AnecdoteList anecdotes={anecdotes} />} />
                 <Route exact path="/about" render={() => <About />} />
                 <Route exact path="/create" render={() => <CreateNew addNew={addNew} />} />
+                <Route
+                    exact
+                    path="/anecdotes/:id"
+                    render={({ match }) => <Anecdote anecdote={anecdoteById(match.params.id)} />}
+                />
                 <Footer />
             </Router>
         </div>
