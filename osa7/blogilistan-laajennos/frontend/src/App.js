c7022bc6efcaec8abad8b7463ff3c5d05fdd9d67
aleksi <a.aleksi.tarvainen@gmail.com>
2019-06-18 17:52:15 +0300

    Osa 7, käyttäjien näkymä

--- osa7/blogilistan-laajennos/frontend/src/App.js
+++ osa7/blogilistan-laajennos/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
@@ -7,6 +8,7 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
+import UsersList from './components/UsersList'
 import { initializeBlogs, addBlog, removeBlog, likeBlog } from './reducers/blogsReducer'
 import { setNotification } from './reducers/notificationReducer'
 import { login, logout } from './reducers/loginReducer'
@@ -97,26 +99,39 @@ const App = props => {
 
     return (
         <div>
-            <Notification />
-            <h2>Blogit</h2>
-            <p>Kirjautuneena: {user.username}</p>
-            <button onClick={handleLogout}>Kirjaudu ulos</button>
-            <h3>Luo uusi</h3>
-            <Togglable buttonText="Uusi blogi">
-                <BlogForm handleCreate={handleCreate} />
-            </Togglable>
-            {props.blogs.map(blog => {
-                const showRemove = user.name === blog.userId.name
-                return (
-                    <Blog
-                        key={blog.id}
-                        blog={blog}
-                        handleLike={handleLike}
-                        handleDelete={handleDelete}
-                        showRemove={showRemove}
+            <Router>
+                <div>
+                    <Notification />
+                    <h2>Blogit</h2>
+                    <p>Kirjautuneena: {user.username}</p>
+                    <button onClick={handleLogout}>Kirjaudu ulos</button>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => (
+                            <div>
+                                <h3>Luo uusi</h3>
+                                <Togglable buttonText="Uusi blogi">
+                                    <BlogForm handleCreate={handleCreate} />
+                                </Togglable>
+                                {props.blogs.map(blog => {
+                                    const showRemove = user.name === blog.userId.name
+                                    return (
+                                        <Blog
+                                            key={blog.id}
+                                            blog={blog}
+                                            handleLike={handleLike}
+                                            handleDelete={handleDelete}
+                                            showRemove={showRemove}
+                                        />
+                                    )
+                                })}
+                            </div>
+                        )}
                     />
-                )
-            })}
+                    <Route exact path="/users" render={() => <UsersList />} />
+                </div>
+            </Router>
         </div>
     )
 }
