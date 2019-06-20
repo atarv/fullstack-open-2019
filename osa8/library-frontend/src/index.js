0e6578dddc7da588609d05c59b4ae14b447f510d
atarv <a.aleksi.tarvainen@gmail.com>
2019-06-20 22:45:44 +0300

    Osa 8, tehtävä 10, kirjan lisäys

---
+++ osa8/library-frontend/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000'
+})
+
+ReactDOM.render(
+    <ApolloHooksProvider client={client}>
+        <App />
+    </ApolloHooksProvider>,
+    document.getElementById('root')
+)
